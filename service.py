07afb45084b9f2c95650b0a6a4190a632f9a3322
Diaa Sami <diaasami@fastmail.fm>
2017-10-21 20:16:09 +0200

    Initial version

---
+++ service.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import tweepy
+import os
+
+
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_SECRET = os.getenv("ACCESS_SECRET")
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+api.wait_on_rate_limit = True
+api.wait_on_rate_limit_notify = True
+
+for tweet in tweepy.Cursor(api.user_timeline).items():
+        print(tweet.created_at, tweet.text, tweet.user.id, tweet.user.followers_count)
